23fb2ebd82ac63f7b4d20776ffdc192f663e7a3d
Simba <leaping96@ajou.ac.kr>
2020-07-06 22:32:44 +0900

    3

--- src/manageCategory.java
+++ src/manageCategory.java
@@ -1,4 +1,11 @@
 
 public class manageCategory {
-	categorySet[] categoryList = new categorySet[999];
+	categorySet[] categoryList = new categorySet[999];	
+	
+	//인자로 들어온 분류 한개에 대해서 하위분류를 리턴하는 함수
+	public String[] showCategory(String category) {
+		//인자가 없으면, 대분류 리스트 리턴
+		
+		//인자가 있으면, 하위분류리스트 리턴
+	}
 }
