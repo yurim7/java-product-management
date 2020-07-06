1b03b612e2cda3c49a80144b64e394a473d10d7d
yurim7 <63902413+yurim7@users.noreply.github.com>
2020-07-06 22:35:45 +0900

    Update categorySet.java

--- src/categorySet.java
+++ src/categorySet.java
@@ -2,4 +2,16 @@ public class categorySet {
 	public String small;
 	public String medium;
 	public String large;
+	
+	public categorySet(String large, String medium, String small){
+		this.large = large;
+		this.medium = medium;
+		this.small = small;
+	}
+	
+	@Override
+	public categorySet(String large, String medium){
+		this.large = large;
+		this.medium = medium;
+	}
 }
